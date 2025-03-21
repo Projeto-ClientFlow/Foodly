eccde1aa258c0f20854c3aca08617f6f565e1426
ELIANEMPONTES <medeiros-eliane1@hotmail.com>
2025-03-20 22:29:56 -0300

    fix: repository conflict

--- src/main/java/com/generation/foodly/repository/CategoriaRepository.java
+++ src/main/java/com/generation/foodly/repository/CategoriaRepository.java
@@ -7,13 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 import com.generation.foodly.model.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-	public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-import com.generation.foodly.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 	public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
--- src/main/java/com/generation/foodly/repository/UsuarioRepository.java
+++ src/main/java/com/generation/foodly/repository/UsuarioRepository.java
@@ -9,11 +9,4 @@ import com.generation.foodly.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 	 public Optional<Usuario> findByUsuario(String usuario);
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.generation.foodly.model.Usuario;
-
-  public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-	 public Optional<Usuario> findByUsuario(String usuario);
-
 }
